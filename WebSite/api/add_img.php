e5690824d8a5d54af92cda9405b2bc97b25082dc
Rekha <rekhamg12@gmail.com>
2020-05-26 22:50:43 +0530

    adding api  files

--- WebSite/api/add_img.php
+++ WebSite/api/add_img.php
@@ -1,7 +1,18 @@
 <?php
 /*https://www.allphptricks.com/upload-file-using-php-save-directory/-codecopy      */
+$entityBody = file_get_contents('php://input');
+  $requestObject=json_decode($entityBody,true);
+  header('Content-Type: application/json');
+ // store request object to database...
 
-$link=mysqli_connect("localhost","root","","tutor");
+  //echo json_encode($requestObject["username"]);
+  //echo json_encode($requestObject["name"]);
+ 
+$fn=$requestObject["img_name"];
+echo json_encode($requestObject["id"]);
+echo json_encode($requestObject["url"]);
+
+/*$link=mysqli_connect("localhost","root","","tutor");
 
 if($link==false)
 {
@@ -9,15 +20,17 @@ if($link==false)
 }
 
 
-if (($_FILES['my_file']['name']!=""))
-{
+//if (($_FILES['my_file']['name']!=""))
+//{
 // Where the file is going to be stored
 $target_dir = "C:/xampp/htdocs/e_class_php_project/WebSite/images/";
-$file = $_FILES['my_file']['name'];
+//$file = $_FILES['my_file']['name'];
+
 $path = pathinfo($file);
 $filename = $path['filename'];
 $ext = $path['extension'];
-$temp_name = $_FILES['my_file']['tmp_name'];
+//$temp_name = $_FILES['my_file']['tmp_name'];
+
 $path_db="";
       if(unlink("c:/users/e_class_php_project/Pictures/saved pictures/"."$file"))
       {
@@ -76,7 +89,7 @@ $rowcount=mysqli_num_rows($res);
                     }
                     }
                     else
-                    {*/
+                    {
    move_uploaded_file($temp_name,$path_filename_ext);
     // $sql="update tab1(img_name,url) VALUES ('$file','$path_db')";
     // $sql="UPDATE tab1 SET img_name='$file',url='$path_db' ,status='$ss' WHERE id=$idd";
@@ -85,17 +98,17 @@ $rowcount=mysqli_num_rows($res);
    $sql="INSERT INTO tab1(img_name,url) VALUES('$filee','$path_db')";
 
      if(mysqli_query($link,$sql)) 
-      {//echo  "Records inserted successfully"; //header("Location:basic-table.php"); 
+      { echo json_encode("Records inserted successfully"); //header("Location:basic-table.php"); 
       } 
       else
       { echo "Error: could not insert"; } 
             //echo "Congratulations! File Uploaded Successfully.";
 
-        header("Location:http://localhost/e_class_php_project/webApp/src/demo_1/index_orig.html");
-  }
+        //header("Location:http://localhost/e_class_php_project/webApp/src/demo_1/index_orig.html");
+  //}
 
 
-
+*/
 
 
 ?>
---
+++ WebSite/api/count_logo.php
@@ -0,0 +1,33 @@
+<?php
+
+$entityBody = file_get_contents('php://input');
+  $requestObject=json_decode($entityBody,true);
+  header('Content-Type: application/json');
+ // store request object to database...
+
+  //echo json_encode($requestObject["username"]);
+  //echo json_encode($requestObject["name"]);
+ 
+  
+$connect = mysqli_connect("localhost", "root", "", "tutor");  
+
+
+if($connect==false){die("error in connection"); }
+$json_array=array();
+
+$page_query = "SELECT * FROM visitors";  
+$page_result = mysqli_query($connect, $page_query);  
+$total_v = mysqli_num_rows($page_result);  
+
+ //echo $total_v;
+$sql="select * from tab1 ";
+$page_result2 = mysqli_query($connect, $sql);  
+$total_l = mysqli_num_rows($page_result2);  
+
+$sql="select * from user ";
+$page_result3 = mysqli_query($connect, $sql);  
+$total_u = mysqli_num_rows($page_result3);  
+$myArr = array($total_v,$total_l,$total_u);
+
+echo json_encode($myArr);
+?>
